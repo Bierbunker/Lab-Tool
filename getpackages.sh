25bf132a08f732c07b9a801f41d4f57523f0f872
etschgi1 <elias.wachmann@gmail.com>
2021-07-09 13:57:49 +0200

    added shell scripts

---
+++ getpackages.sh
@@ -0,0 +1,8 @@
+#install needed packages
+pip3 install pandas
+pip3 install numpy
+pip3 install matplotlib
+pip3 install uncertainties
+
+#run the skricpt
+python3 src/main.py
---
+++ run.sh
@@ -0,0 +1,2 @@
+#run the skricpt
+python3 src/main.py
