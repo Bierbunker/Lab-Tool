c48ad217d4e2009a9a0b5892338134858e077d3f
Philipp, Maximilian <philipp@student.tugraz.at>
2021-10-11 00:03:08 +0200

    testing some more of my shit

--- test.py
+++ test.py
@@ -8,8 +8,11 @@ if __name__ == "__main__":
     P.load_data("./src/labtool_ex2/Data/abbe/nadatablue")
     print(P.data)
     na = Equation(
-        "r / f", "r f",P.name, label="Numerische Apertur", dataframe=P.data[["r", "f"]]
+        "r / f", "r f", P.name, label="Numerische Apertur", dataframe=P.data[["r", "f", 'dr','df']]
     )
+    print(na.err_expr)
+    P.data["NA"] = na.apply_df()
+    P.data["dNA"] = na.apply_df_err()
     P.equations["NA"] = na
-    na.plot_data_scatter("f", labels=["bro", "test"])
+    na.plot_data_scatter("r", errors=True, labels=["r / mm", "N.A. / 1"])
 print(dir())
