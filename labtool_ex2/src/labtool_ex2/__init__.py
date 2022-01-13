c5bc0ba66c025f6eb02dd1ef5e048e55f7b9d8e2
Maximilian Philipp <philipp@student.tugraz.at>
2022-01-13 19:38:24 +0100

    monkeypatch andi

--- labtool_ex2/src/labtool_ex2/__init__.py
+++ labtool_ex2/src/labtool_ex2/__init__.py
@@ -4,3 +4,6 @@ __version__ = "0.0.1"
 from .Equation import *
 from .Project import *
 from .Project import Project as Project  # noqa
+from . import monkeypatch_uncertainties as mpatch
+
+mpatch.display()
