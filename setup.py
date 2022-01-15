414fcbf4aa5c471d058b2accadf7a50dd5c3929c
Maximilian Philipp <philipp@student.tugraz.at>
2022-01-15 23:26:01 +0100

    fixed package_data stuff now we have some data

--- setup.py
+++ setup.py
@@ -5,13 +5,4 @@ import sys
 site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 if __name__ == "__main__":
-    setup_args = {
-    'packages': ['labtool_ex2_data','labtool_ex2_data.input'],
-    'package_dir': {
-        'labtool_ex2_data': 'labtool_ex2_data',
-        'labtool_ex2_data.input':'labtool_ex2_data/input'
-    },
-    'package_data':{'labtool_ex2_data':['labtool_ex2_data/*.bib','labtool_ex2_data/*.tex'],'labtool_ex2_data.input':['labtool_ex2_data/input/*tex']},
-    'include_package_data': True
-    }
-    setup(**setup_args)
+    setup()
