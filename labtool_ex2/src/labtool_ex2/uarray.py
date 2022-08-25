809631d878caf32b6d44a76638541b90553480f1
Maximilian Philipp <philipp@student.tugraz.at>
2022-08-25 11:08:42 +0200

    formatting

--- labtool_ex2/src/labtool_ex2/uarray.py
+++ labtool_ex2/src/labtool_ex2/uarray.py
@@ -6,18 +6,18 @@ class UArray(ndarray):
         # Input array is an already formed ndarray instance
         # We first cast to be our class type
         return asarray(input_array).view(cls)
-    
+
     @property
     def n(self):
         return asarray([catch(lambda x: x.n, value) for value in self])
+
     # ufloat_from_str(x.__format__()).n
-    
+
     @property
     def s(self):
-        return asarray([catch(lambda x: x.s, value, handle=lambda _: 0) for value in self])
-    
-    
-
+        return asarray(
+            [catch(lambda x: x.s, value, handle=lambda _: 0) for value in self]
+        )
 
 
 # https://stackoverflow.com/questions/1528237/how-to-handle-exceptions-in-a-list-comprehensions
