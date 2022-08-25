2f22ca19e42863a3ecbd937710b5e0a82ce7b284
Maximilian Philipp <philipp@student.tugraz.at>
2022-08-25 17:33:29 +0200

    automatic conversion from str to ufloat if possible optimisation NEEDED

--- labtool_ex2/src/labtool_ex2/uarray.py
+++ labtool_ex2/src/labtool_ex2/uarray.py
@@ -1,10 +1,21 @@
 from numpy import asarray, ndarray
+from uncertainties import ufloat_fromstr
 
 
 class UArray(ndarray):
     def __new__(cls, input_array):
         # Input array is an already formed ndarray instance
         # We first cast to be our class type
+        if hasattr(input_array, "dtype"):
+            # print(input_array.dtype)
+            for val in input_array:
+                if isinstance(val, str):
+                    try:
+                        input_array = [ufloat_fromstr(x) for x in input_array]
+                    except ValueError:
+                        print(f"Warning conversion failed for {input_array}")
+                    break
+
         return asarray(input_array).view(cls)
 
     @property
